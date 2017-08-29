9c2db61c51ae5b4a9d5bb7f6804b88b8a9ab67f7
menyin <845257580@qq.com>
2017-08-29 08:34:01 +0800

    注释添加

--- ui795img/crm/min/js/v2/base/util.js
+++ ui795img/crm/min/js/v2/base/util.js
@@ -38,6 +38,11 @@ define('base.util', function(require, exports){
 		isNumber: function(o){
 			return toString.call(o) === '[object Number]';
 		},
+		/**
+		 * 判断是否是纯的js对象（即非dom对象）
+		 * @param o 对象
+		 * @returns {boolean}
+		 */
 		isPlainObject: function(o){
 			return o && this.isObject(o) && 
 				!o['noteType'] && !this.isWindow(o);
@@ -112,6 +117,9 @@ define('base.util', function(require, exports){
 	};
 	exports.array = jpjs.arrayUtil;
 	exports.object = {
+		/**
+		 * 获取传入参数对象的非原型属性的属性名数组
+		 */
 		keys: Object.keys || function (o) {
 			var result = [], p, i;
 			for (p in o) {
@@ -131,15 +139,43 @@ define('base.util', function(require, exports){
 		}
 	};
 	exports.string = {
+		/**
+		 * 去除空格
+		 * @param str 原字符串
+		 * @returns {*} 处理后的字符串
+		 */
 		trim: function(str){
 			return str === undefined ? '' : str.toString().replace(reg_trim, '');
 		},
+		/**
+		 * 对传入字符串进行编码
+		 * @param val 字符串
+		 * @returns {string|XML} 编码后的字符串
+		 */
 		escape: function(val) {
 			return val.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&acute;');
 		},
+		/**
+		 * 对传入字符串进行解码
+		 * @param val 字符串
+		 * @returns {string|XML} 解码后的字符串
+		 */
 		unescape: function(val) {
 			return val.replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"').replace(/&amp;/g, '&').replace(/&acute;/g, '\'');
 		},
+		/**
+		 * 字符串替换功能
+		 * @param string 模板字符串，其中用{name}来表示变量
+		 * @param obey 数据对象或被替换的字符串数组
+		 * @param aval 替换的付出数组
+		 * @returns {*} 被替换处理后的字符串
+		 * @remark
+		 *  1、当2个参数时，该函数类似但不等于art-template的功能，2个参数依次是模板字符串和数据对象或数组
+		 *  2、当3各参数时，该函数功能为：处理模板字符串，将该模板里和obey数组元素匹配的字符串依次
+		 *     替换成aval数组里的元素。此时obey和aval应该都是字符串数组，如果obey为字符串，则会被替
+		 *     换为[obey]的数组，如果aval为字符串，也是同理。
+		 *   注意：模板中只能替换obey和aval中为基本类型的第一层属性值
+		 */
 		replace: function(string, obey, aval){
 			if(arguments.length == 2){
 				//简单模板的情况
@@ -159,6 +195,13 @@ define('base.util', function(require, exports){
 			}
 			return string;
 		},
+		/**
+		 * 将参数对象参数转化为字符串
+		 * @param o 参数对象
+		 * @returns {string} 转化后的字符串
+		 * @example
+		 *  var str = util.string.param({name: 'cny', age: 11});//输出'name=cny&age=11'
+		 */
 		param: function(o){
 			if(!exports.type.isPlainObject(o)) return '';
 			var buf = [], key, val;
@@ -178,6 +221,14 @@ define('base.util', function(require, exports){
 			buf.pop();
 			return buf.join('');
 		},
+		/**
+		 * 将参数字符串转化为对象
+		 * @param str 参数字符串
+		 * @param sep 参数项的分割符，不传默认'&'
+		 * @returns {{}} 生成的参数对象
+		 * @example
+		 * var str = util.string.unparam('name=cny&age=11');//输出{name: 'cny', age: 11}
+		 */
 		unparam: function(str, sep){
 			if(exports.type.isEmptyString(str) || !exports.type.isString(str))
 				return {};
@@ -206,6 +257,17 @@ define('base.util', function(require, exports){
 			return ret;
 		}
 	};
+	/**
+	 * 在原有的t对象上拷贝s对象的属性，并可以指定是否同名覆盖和属性列表
+	 * @param t 要拷贝的原有对象
+	 * @param s 被拷贝属性的对象
+	 * @param ov {boolea} 默认值true 为true时z表示s在wl指定的属性与t同名时，要用s覆盖t的
+	 * @param wl 指定s对象要复制到t对象的属性的数组
+	 * @returns {*}
+	 * @remark
+	 *  1、ov和wl应该要同时出现，用于指定复制的属性及是否覆盖
+	 *  2、wl不指定时，则t会复制s所有属性，至于要不要同名覆盖则由ov决定
+	 */
 	exports.mix = function(t, s, ov, wl){
 		if (!s || !t) 
 			return t;
@@ -230,6 +292,12 @@ define('base.util', function(require, exports){
 		}
 		return t;
 	};
+	/**
+	 * 让t类型继承s类型
+	 * @param t 目标子类
+	 * @param s 目标父类
+	 * @returns 返回t继承s后的对象
+	 */
 	exports.extend = function(t, s){
 		if(!s || !t)
 			return t;
@@ -250,11 +318,19 @@ define('base.util', function(require, exports){
 			sp.constructor = s;
 		return t;
 	};
+	/**
+	 * 合并两个对象的属性
+	 * @param r
+	 * @param s
+	 * @returns {*}
+	 * @remark
+	 * 其中s对象的属性会覆盖r的同名属性
+	 */
 	exports.merge = function(r, s){
 		var key, value;
 		for (key in s) {
 			try {
-				if (s.hasOwnProperty(key)) {
+				if (s.hasOwnProperty(key)) {//判断s是否有key属性
 					r[key] = this.clone(s[key], r[key]);
 				}
 			} catch(ex) {
@@ -265,6 +341,17 @@ define('base.util', function(require, exports){
 		
 		return r;
 	};
+	/**
+	 * 在r对象中拷贝s对象的属性
+	 * @param r
+	 * @param s
+	 * @returns {*}
+	 * @remark
+	 *  1、r,s必须是纯js对象(非dom对象)
+	 *  2、r的同名属性不会被s覆盖
+	 *  3、注意以上两点是和merge函数的区别
+	 *  4、如果r是数组则直接返回r
+	 */
 	exports.clone = function(r, s){
 		if (this.type.isArray(r)) {
 			r = r.slice();
@@ -274,6 +361,23 @@ define('base.util', function(require, exports){
 		}
 		return r;
 	};
+	/**
+	 * 获取一个绑定具体对象的委托实例
+	 * @param 自由参数，第1参为回调，第2参为对象
+	 * @returns {Function}
+	 * @example
+	 * obj={};
+	 * obj.myMethod=function(datas){
+	 *   //这个函数相当于一个事件函数
+	 *   //datas数组包含了几部分参数
+	 *   //1、除了下面util.bind()调用时第一二参数以外的参数(一般没有传一二参以外的参数)
+	 *   //2、以下callback所传递的data数组里的参数
+	 * }
+	 * $.get('test.php',{},function(data){
+	 *  var callback= util.bind(obj.myMetod, obj);
+	 *  callback(data);
+	 * })
+	 */
 	exports.bind = function(){
 		var args = slice.call(arguments) || [],
 			fun = args.shift(),
@@ -284,6 +388,9 @@ define('base.util', function(require, exports){
 			fun.apply(obj, ar.concat(args));
 		}
 	};
+	/**
+	 * 将字符串转化为json对象的函数
+	 */
 	exports.json = window.JSON ? JSON.parse : function(text, mode){
 		if(mode){
 			return (new Function( "return " + text ) )();
